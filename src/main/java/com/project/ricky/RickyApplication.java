23cc455151c130cb341b3100f396cbd781510172
명호 <kimzzan1234@gmail.com>
2022-08-21 19:44:40 +0900

    Initial commit

---
+++ src/main/java/com/project/ricky/RickyApplication.java
@@ -0,0 +1,13 @@
+package com.project.ricky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ricky/RickyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ricky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
