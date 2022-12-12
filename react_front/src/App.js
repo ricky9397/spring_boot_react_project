e5553eb5335a673d3c9af2a8aa711acd30953a71
ricky <kimzzan1234@gmail.com>
2022-12-13 02:12:31 +0900

    배포실패로인한 새로운 react 프로젝트

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        리액트 화면디자인좀만들어줄사람.....? 제발...
+          조빵 메 야 ~
+        리액트 화면디자인좀만들어주라~ 제발...
       </header>
     </div>
   );
