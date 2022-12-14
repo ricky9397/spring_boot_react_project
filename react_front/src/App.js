8775f00c21692abc93ea214a33007537762ced83
ricky <kimzzan1234@gmail.com>
2022-12-15 01:15:09 +0900

    dd

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+/* import './App.css';
 import Login from './components/AuthForm';
 
 function App() {
   return (
     <Login />
+  );
+}
+
+export default App; */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
