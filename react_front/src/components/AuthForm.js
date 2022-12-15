eb0835ce3470e67b06290f63301d1f5223bb9fae
ricky <kimzzan1234@gmail.com>
2022-12-15 21:19:46 +0900

    docker file 수정

--- react_front/src/components/AuthForm.js
+++ react_front/src/components/AuthForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../assets/styles/login.css";
 import { GoogleLogin } from "react-google-login";
 
-function AuthForm() {
+const AuthForm = () => {
   return (
     <div className="login">
       <h4>떡꿍</h4>
